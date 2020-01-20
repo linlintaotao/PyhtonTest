352726c35cff20aedc1464321fb9f9ea31febdf0
lintao zhang <lt.zhang@feyman.cn>
2020-01-20 14:34:14 +0800

    second commit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # 测试入口
 from src.analysis.Analysis import AnalysisTool
+import os
 
 if __name__ == '__main__':
-    analysisTool = AnalysisTool()
+    analysisTool = AnalysisTool(os.path.abspath('./data/'))
     analysisTool.read_file()
     analysisTool.analysis()
     pass
