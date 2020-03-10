a2eba5d565ebf67383aa7a5edd40d58bd07778c7
lintao zhang <lt.zhang@feyman.cn>
2020-03-10 19:03:42 +0800

    add word report -commit

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ import sys
 # for path in sys.path:
 #     sys.path.append(path)
 
-sys.path.append("\\Users\\lintao\\PycharmProjects//PyhtonTest")
-sys.path.append("\\usr\\local\\Cellar\\python\\3.7.5\\Frameworks\\Python.framework\\Versions\\3.7\\lib\\python37.zip")
-sys.path.append("\\usr\\local\\Cellar\\python\\3.7.5\\Frameworks\\Python.framework\\Versions\\3.7\\lib\\python3.7")
-sys.path.append("\\usr\\local\\Cellar\\python\\3.7.5\\Frameworks\\Python.framework\\Versions\\3.7\\lib\\python3.7\\lib-dynload")
-sys.path.append("\\Users\\lintao\\Library\\Python\\3.7\\lib\\python\\site-packages")
-sys.path.append("\\usr\\local\\lib\\python3.7\\site-packages")
+sys.path.append("//Users//lintao//PycharmProjects//PyhtonTest")
+sys.path.append("//usr//local//Cellar//python//3.7.5//Frameworks//Python.framework//Versions//3.7//lib//python37.zip")
+sys.path.append("//usr//local//Cellar//python//3.7.5//Frameworks//Python.framework//Versions//3.7//lib//python3.7")
+sys.path.append("//usr//local//Cellar//python//3.7.5//Frameworks//Python.framework//Versions//3.7//lib//python3.7//lib-dynload")
+sys.path.append("//Users//lintao//Library//Python//3.7//lib//python//site-packages")
+sys.path.append("//usr//local//lib//python3.7//site-packages")
 from src.analysis.Analysis import AnalysisTool
 
 if __name__ == '__main__':
     print(sys.path)
     #
-    # analysisTool = AnalysisTool(os.path.abspath('.\\data\\'))
+    analysisTool = AnalysisTool(os.path.abspath('.//data//'))
     # analysisTool.read_file()
     # analysisTool.analysis()
     print('Hello world')
