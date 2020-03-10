99d6b5f884ae378d94c5402eb3135e8c7a052eba
lintao zhang <lt.zhang@feyman.cn>
2020-03-10 12:28:37 +0800

    add word report -commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # coding= utf-8
 # 测试入口
-from src.analysis.Analysis import AnalysisTool
-import os
+# import os
+# from src.analysis.Analysis import AnalysisTool
 
 if __name__ == '__main__':
     # analysisTool = AnalysisTool(os.path.abspath('./data/'))
