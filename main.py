2693b7d6d960f8037d32300ce781ba11060523be
lintao zhang <lt.zhang@feyman.cn>
2020-03-11 16:09:06 +0800

    add word report -commit

--- main.py
+++ main.py
@@ -3,10 +3,6 @@
 
 import os
 import sys
-import sys
-
-print('Python %s on %s' % (sys.version, sys.platform))
-sys.path.extend([WORKING_DIR_AND_PYTHON_PATHS])
 from src.analysis.Analysis import AnalysisTool
 
 if __name__ == '__main__':
