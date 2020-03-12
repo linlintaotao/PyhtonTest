048d7c40c805edbbfe363f7b1feab095140553bc
lintao zhang <lt.zhang@feyman.cn>
2020-03-12 15:10:15 +0800

    add main.py -commit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 # coding= utf-8
 # 测试入口
-
 import os
-import sys
 from src.analysis.Analysis import AnalysisTool
 from src.streams.manger import Manager
 
@@ -14,7 +12,6 @@ def startTest():
 
 if __name__ == '__main__':
     startTest()
-
     # analysisTool = AnalysisTool(os.path.abspath('./data/'))
     # analysisTool.read_file()
     # analysisTool.analysis()
