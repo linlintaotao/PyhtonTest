8be664c32522b6550d509418d678ff38546dc562
lintao zhang <lt.zhang@feyman.cn>
2021-01-19 10:45:43 +0800

    add postProcess -commit by zlt

--- PPMain.py
+++ PPMain.py
@@ -6,7 +6,7 @@ from src.analysis.postProcess import PostProcess
 def start(argv):
     if len(argv) <= 1:
         return
-    pp = PostProcess(truthPath=os.path.join(argv[1], 'truth'), ppPath=os.path.join(argv[1], 'PP'))
+    pp = PostProcess(truthPath=os.path.join(argv[1], 'Truth'), ppPath=os.path.join(argv[1], 'PP'))
     pp.startAnalysis()
 
 
